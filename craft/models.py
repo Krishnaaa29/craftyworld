bf457c5a0b49b34995bd4a844943b34e9bdf38e5
Alfin Jiji <alfinjiji2@gmail.com>
2021-01-08 13:20:28 +0530

    cart order

--- craft/models.py
+++ craft/models.py
@@ -1,6 +1,7 @@
 from craft import db, login_manager, app
 from flask_login import UserMixin
-
+from datetime import datetime
+import json
 
 @login_manager.user_loader
 def load_user(user_id):
@@ -15,7 +16,7 @@ class Users(db.Model, UserMixin):
 
     def __repr__(self):
         return f"Users('{self.username}', '{self.email}')"
-
+"""
 class Admin(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(60), nullable=False)
@@ -24,7 +25,7 @@ class Admin(db.Model):
 
     def __repr__(self):
         return f"Admin('{self.name}', '{self.email}')"
-
+"""
 class Products(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(60), nullable=False)
@@ -61,4 +62,33 @@ class Feedback(db.Model):
     def __repr__(self):
         return f"Feedback('{self.name}', '{self.email}', '{self.message}')"
 
+# cart order table
+class JsonEcodedDict(db.TypeDecorator):
+    impl = db.Text
+    def process_bind_param(self, value, dialect):
+        if value is None:
+            return '{}'
+        else:
+            return json.dumps(value)
+    def process_result_value(self, value, dialect):
+        if value is None:
+            return '{}'
+        else:
+            return json.loads(value)
 
+class CustomerOrder(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    invoice = db.Column(db.String(20), unique=True, nullable=False)
+    status = db.Column(db.String(20), default='pending', nullable=False)
+    customer_id = db.Column(db.Integer, unique=False, nullable=False)
+    date_created = db.Column(db.DateTime(20), default=datetime.utcnow, nullable=False)
+    orders = db.Column(JsonEcodedDict)
+    name = db.Column(db.String(60), nullable=False)
+    address = db.Column(db.String(60), nullable=False)
+    pincode = db.Column(db.Integer, nullable=False)
+    city = db.Column(db.String(60), nullable=False)
+    state = db.Column(db.String(60), nullable=False)
+    mobile = db.Column(db.Integer, nullable=False)
+
+    def _repr_(self):
+        return'<customerOrder %r>' %self.invoice
