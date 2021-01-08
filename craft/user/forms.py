f9a938419fe525af75e557f2072f55c6b4e8487e
Alfin Jiji <alfinjiji2@gmail.com>
2021-01-08 13:40:38 +0530

    feedback design'

--- craft/user/forms.py
+++ craft/user/forms.py
@@ -3,7 +3,7 @@ from flask_login import current_user
 from wtforms import StringField, PasswordField, SubmitField, BooleanField, TextAreaField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from craft.models import Users
-
+from wtforms.widgets import TextArea
 
 
 
@@ -68,5 +68,5 @@ class ResetPasswordForm(FlaskForm):
 class FeedbackForm(FlaskForm):
     name = StringField('Name', validators=[DataRequired(), Length(min=2, max=20)])
     email = StringField('Email', validators=[DataRequired(), Email()])
-    message = TextAreaField('Message', validators=[DataRequired()])
+    message = TextAreaField('Message', widget=TextArea(), validators=[DataRequired()])
     submit = SubmitField('Send Message')
